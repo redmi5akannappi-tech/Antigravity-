5db316a2318e3c86704cda07637371d4b07eb80b
redmi-derosky <redmi5akannappi@gmail.com>
2026-01-14 10:57:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-import antigravity  # built-in, do NOT install
+import antigravity  # built-in, no install needed
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Antigravity imported successfully. Deployment is working."
+    return "🚀 Flask is installed. Antigravity imported. Render deploy works."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-  
+    
